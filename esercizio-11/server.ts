638eaf0e63d657980d0ef009d9dd1d75fd1b7bd9
Maurilio Farina <farinamaurilio93@gmail.com>
2023-07-07 11:29:50 +0200

    esercizio-13:complete

--- esercizio-11/server.ts
+++ esercizio-11/server.ts
@@ -1,25 +1,76 @@
-import express from 'express';
-import "express-async-error";
-import morgan from "morgan";
-import { getAll, getOneById, create, updateById, deleteById } from "./controllers/planets.js";
+import Joi from 'joi';
+import pgPromise from 'pg-promise';
 
-const app = express();
-const port = 3000;
+const db = pgPromise()("postgres://postgres:postgres98@localhost:5432/planets");
 
-app.use(morgan("dev"));
-app.use(express.json());
+//ex-13
+const setupDb = async () => {
+  await db.none(`
+  DROP TABLE IF EXISTS planets;
 
+  CREATE TABLE planets (
+    id SERIAL NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL
+  );
+  `)
 
-app.get("/api/planets", getAll);
+  await db.none(`INSERT INTO planets (name) VALUES ('Earth')`)
+  await db.none(`INSERT INTO planets (name) VALUES ('Mars')`)
+}
 
-app.get("/api/planets/:id", getOneById);
+setupDb()
 
-app.post('/api/planets', create);
+const getAll = async (req, res) => {
+  const planets = await db.many(`SELECT * FROM planets`)
+    res.status(200).json(planets)
+}
 
-app.put('/api/planets/:id', updateById);
+const getOneById = async (req, res) => {
+    const {id} = req.params;
+    const planet = await db.oneOrNone(`SELECT * FROM planets WHERE id=$1;`, Number(id))
+    res.status(200).json(planet);
+  }
 
-app.delete('/api/planets/:id', deleteById);
+const planetSchema = Joi.object({
+    name: Joi.string().required(),
+})
 
-app.listen(port, () => {
-    console.log(`Example app listening on port http://localhost:${port}`);
-});
+const create = async (req, res) => {
+    const {name} = req.body
+    const newPlanet = {name}
+    const validatedNewPlanet = planetSchema.validate(newPlanet)
+
+    if (validatedNewPlanet.error) {
+        return res.status(400).json({ msg: validatedNewPlanet.error.details[0].message })
+    } else {
+      await db.none(`INSERT INTO planets (name) VALUES ($1)`, name)
+      res.status(201).json({msg: "The planet was created."})
+    }
+  }
+
+const updateById = async (req, res) =>{
+    const {id} = req.params
+    const {name} = req.body
+    await db.none(`UPDATE planets SET name=$2 WHERE id=$1`, [id, name])
+    res.status(200).json({msg: "The planet was updated"})
+  }
+
+const deleteById = async (req, res) =>{
+    const {id} = req.params;
+    await db.none(`DELETE FROM planets WHERE id=$1`, Number(id))
+    res.status(200).json({ msg: "The planet was deleted"});
+  }
+
+  //ex-14
+  const createImage = async (req, res) =>{
+    const {id} = req.params;
+    const filename = req.file?.path;
+    if (filename) {
+      db.none("UPDATE planets SET image=$2 WHERE id=$1", [id, filename])
+      res.status(201).json({msg: "Planet image uploded successfully"})
+    }else{
+      res.status(400).json({msg : "Planet image failed to upload"})
+    }
+  }
+
+export { getAll, getOneById, create, updateById, deleteById, createImage}
