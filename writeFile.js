f4df42e85c16df80c93131d5ad0f39456851483f
Maurilio Farina <farinamaurilio93@gmail.com>
2023-07-07 10:34:43 +0200

    esercizio-8:complete

--- writeFile.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-
-fs.writeFile('file.txt', 'hello worlddddddd', 'utf8', (err) => {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log('file written')
-    }
-})
