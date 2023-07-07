c2b7c1f5511f2b790e02c49597064552ce4e9282
Maurilio Farina <farinamaurilio93@gmail.com>
2023-07-07 10:37:04 +0200

    esercizio-9:complete

---
+++ getResult.mjs
@@ -0,0 +1,28 @@
+function luckyDraw(player) {
+  return new Promise((resolve, reject) => {
+    const win = Boolean(Math.round(Math.random()));
+
+    process.nextTick(() => {
+      if (win) {
+        resolve(`${player} won a prize in the draw!`);
+      } else {
+        reject(new Error(`${player} lost the draw.`));
+      }
+    });
+  });
+}
+
+async function getResult() {
+  try {
+      const response = await luckyDraw('Tina');
+      console.log(response)
+      const response2 = await luckyDraw('Jorge');
+      console.log(response2)
+      const response3 = await luckyDraw('Julien')
+      console.log(response3)
+  } catch (error) {
+      console.log(error)
+  }
+}
+
+getResult()
